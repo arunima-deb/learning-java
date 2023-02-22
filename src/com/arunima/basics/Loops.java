85b6438ac932d672d55ebd7af6b0029934409792
Arunima Deb <arunima.s.deb@gmail.com>
2023-02-22 23:47:21 +0530

    First commit

---
+++ src/com/arunima/basics/Loops.java
@@ -0,0 +1,13 @@
+package com.arunima.basics;
+
+/**
+ * This class prints geometrical shapes like triangles, squares etc.
+ * by using loops.
+ */
+public class Loops {
+
+	public static void main(String[] args) {
+		System.out.println( "All rise and kneel. This is Arunima coming into " +
+	                        "the world of Java programming." ) ;
+	}
+}
