75c19e1a2526ede1298b267d3258c4e7d2cbd277
Sandeep Deb <deb.sandeep@gmail.com>
2023-04-07 16:26:05 +0530

    Test cases

---
+++ src/com/arunima/basics/operators/InclinedMirrorTest.java
@@ -0,0 +1,18 @@
+package com.arunima.basics.operators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals ;
+
+import org.junit.jupiter.api.Test ;
+
+class InclinedMirrorTest {
+    
+    InclinedMirrors mirrors = new InclinedMirrors() ;
+
+    @Test
+    void testGetNumImages() {
+        assertEquals(  3, mirrors.getNumImages( 90 ) ) ;
+        assertEquals(  5, mirrors.getNumImages( 60 ) ) ;
+        assertEquals(  4, mirrors.getNumImages( 72 ) ) ;
+        assertEquals( 11, mirrors.getNumImages( 30 ) ) ;
+    }
+}
