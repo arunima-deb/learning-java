791d9f41611de9065e85c113076809e164a76666
Sandeep Deb <deb.sandeep@gmail.com>
2023-04-07 16:36:34 +0530

    Enhanced test cases

--- src/com/arunima/basics/operators/InclinedMirrorTest.java
+++ src/com/arunima/basics/operators/InclinedMirrorTest.java
@@ -11,8 +11,13 @@ class InclinedMirrorTest {
     @Test
     void testGetNumImages() {
         assertEquals(  3, mirrors.getNumImages( 90, false ) ) ;
+        assertEquals(  3, mirrors.getNumImages( 90,  true ) ) ;
         assertEquals(  5, mirrors.getNumImages( 60, false ) ) ;
-        assertEquals(  4, mirrors.getNumImages( 72, false ) ) ;
+        assertEquals(  5, mirrors.getNumImages( 72, false ) ) ;
+        assertEquals(  4, mirrors.getNumImages( 72,  true ) ) ;
         assertEquals( 11, mirrors.getNumImages( 30, false ) ) ;
+        assertEquals( 11, mirrors.getNumImages( 30,  true ) ) ;
+        assertEquals(  5, mirrors.getNumImages( 55,  true ) ) ;
+        assertEquals(  6, mirrors.getNumImages( 55, false ) ) ;
     }
 }
